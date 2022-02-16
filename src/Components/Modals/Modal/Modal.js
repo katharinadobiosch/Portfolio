8800ddcabd2ef2c5704a19b5c92ee53f3a6039be
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2022-02-16 09:34:58 +0100

    added json file with peoject content. added map to project.js

---
+++ src/Components/Modals/Modal/Modal.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "../../Modals/FreshbnbModal/FreshbnbModal.module.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+const Modal = (props) => {
+    if (!props.show) {
+        return null;
+    }
+
+    const stop = (event) => {
+        event.stopPropagation();
+    };
+
+    return (
+        <>
+            <div onClick={stop}>
+                <div className={styles.modal} onClick={props.onClose}>
+                    <div className={styles["modal-container"]}>
+                        <div className={styles["modal-header"]}>
+                            <div
+                                className={styles["modal-close"]}
+                                onClick={props.onClose}
+                            >
+                                <FontAwesomeIcon icon={faXmark} />
+                            </div>
+                        </div>
+                        <div className={styles["modal-body"]}>
+                            <iframe
+                                title="Freshbnb"
+                                src="https://freshbnb.vercel.app/"
+                            ></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Modal;
--- src/Views/Projects/Projects.js
+++ src/Views/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Projects/Projects.module.scss";
 
 import Freshbnb from "../../Components/Freshbnb/Freshbnb";
@@ -7,40 +7,156 @@ import ExpensesApp from "../../Components/ExpensesApp/ExpensesApp";
 import FoodOrderApp from "../../Components/FoodOrderApp/FoodOrderApp";
 import BlogLayout from "../../Components/BlogLayout/BlogLayout";
 
+import projectData from "../Projects/projectData.json";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+// import FreshbnbImg from "../Projects/static/freshbnb.png";
+import FreshbnbModal from "../../Components/Modals/FreshbnbModal/FreshbnbModal";
+import Modal from "../../Components/Modals/Modal/Modal";
+
 const Projects = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = (event) => {
+        event.preventDefault();
+        setShowModal(true);
+    };
+
+    const closeModal = (event) => {
+        event.preventDefault();
+        setShowModal(false);
+    };
+
     return (
         <>
-            <div id="projects" className={styles["main-container"]}>
-                {/* // ?? OPENING TAG */}
-                <div className={styles["opening-tag"]}>
-                    <span>&lt;div </span>
-                    <span>id</span>=<span>"projects"</span>
-                    <span>&gt;</span>
-                </div>
-                {/* // ?? CONTENT */}
-                <div className={styles["content-container"]}>
-                    <div className={styles.content}>
-                        {/* PROJECTS PROJECTS PROJECTS{" "} */}
+            {projectData.map((project, index) => {
+                return (
+                    <div id="projects" className={styles["main-container"]}>
+                        {/* // ?? OPENING TAG */}
+                        <div className={styles["opening-tag"]}>
+                            <span>&lt;div </span>
+                            <span>id</span>=<span>"projects"</span>
+                            <span>&gt;</span>
+                        </div>
+                        {/* // ?? CONTENT */}
+                        <div className={styles["content-container"]}>
+                            <div className={styles.content}>
+                                {/* PROJECTS PROJECTS PROJECTS{" "} */}
 
-                        <div>
-                            <Freshbnb />
-                            <BurgerBuilder />
-                            <ExpensesApp />
-                            <FoodOrderApp />
-                            <BlogLayout />
+                                {/* // MAP THROUGH JSON DATA */}
+                                <div>
+                                    <div
+                                        className={styles["content-container"]}
+                                    >
+                                        <div
+                                            className={
+                                                styles["image-container"]
+                                            }
+                                            onClick={openModal}
+                                        >
+                                            <img
+                                                src={project.image}
+                                                alt="Freshbnb"
+                                                onClick={openModal}
+                                            />
+                                            <Modal
+                                                onClose={closeModal}
+                                                show={showModal}
+                                            />
+                                        </div>
+
+                                        <div className={styles.content}>
+                                            <div className={styles.heading}>
+                                                <a
+                                                    href={project.link}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    {project.title}
+                                                </a>
+
+                                                <a
+                                                    href={project.gitHubLink}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    <FontAwesomeIcon
+                                                        icon={faGithub}
+                                                    />
+                                                </a>
+                                            </div>
+
+                                            <div className={styles.description}>
+                                                {project.description}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className={styles["burger-creator"]}></div>
+                            </div>
                         </div>
 
-                        <div className={styles["burger-creator"]}></div>
+                        {/* // ?? CLOSING TAG */}
+                        <div className={styles["closing-tag"]}>
+                            <span>&lt;/div&gt;</span>
+                        </div>
                     </div>
-                </div>
-
-                {/* // ?? CLOSING TAG */}
-                <div className={styles["closing-tag"]}>
-                    <span>&lt;/div&gt;</span>
-                </div>
-            </div>
+                );
+            })}
         </>
     );
 };
 
 export default Projects;
+
+//////////////////////////////////////////////////////////////////////
+
+// import React from "react";
+// import styles from "../Projects/Projects.module.scss";
+
+// import Freshbnb from "../../Components/Freshbnb/Freshbnb";
+// import BurgerBuilder from "../../Components/BurgerBuilder/BurgerBuilder";
+// import ExpensesApp from "../../Components/ExpensesApp/ExpensesApp";
+// import FoodOrderApp from "../../Components/FoodOrderApp/FoodOrderApp";
+// import BlogLayout from "../../Components/BlogLayout/BlogLayout";
+
+// const Projects = () => {
+//     return (
+//         <>
+//             <div id="projects" className={styles["main-container"]}>
+//                 {/* // ?? OPENING TAG */}
+//                 <div className={styles["opening-tag"]}>
+//                     <span>&lt;div </span>
+//                     <span>id</span>=<span>"projects"</span>
+//                     <span>&gt;</span>
+//                 </div>
+//                 {/* // ?? CONTENT */}
+//                 <div className={styles["content-container"]}>
+//                     <div className={styles.content}>
+//                         {/* PROJECTS PROJECTS PROJECTS{" "} */}
+
+//                         <div>
+//                             <Freshbnb />
+//                             <BurgerBuilder />
+//                             <ExpensesApp />
+//                             <FoodOrderApp />
+//                             <BlogLayout />
+//                         </div>
+
+//                         <div className={styles["burger-creator"]}></div>
+//                     </div>
+//                 </div>
+
+//                 {/* // ?? CLOSING TAG */}
+//                 <div className={styles["closing-tag"]}>
+//                     <span>&lt;/div&gt;</span>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default Projects;
